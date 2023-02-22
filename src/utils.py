a9ccd4eb468a860a3f7ad8513c7a7674df72b8f2
gersongerardcruz <ggerardcruz@gmail.com>
2023-02-22 19:59:27 +0800

    Add remove id functionality

--- src/utils.py
+++ src/utils.py
@@ -349,3 +349,20 @@ def encode_df(df, encoder_type, train: bool):
                 raise ValueError("Encoder type must be one of 'onehot', 'ordinal', 'label', or 'binary'.")
 
     return df_encoded, mappings
+
+def remove_id_column(df):
+    """
+    Removes the ID column from a pandas dataframe.
+    Takes into account different spellings of "ID" and "Unnamed: 0".
+
+    Parameters:
+        df (pandas.DataFrame): The input dataframe.
+
+    Returns:
+        pandas.DataFrame: The dataframe with the ID column removed.
+    """
+    id_columns = ['ID', 'Id', 'id', 'Unnamed: 0']
+    for col in id_columns:
+        if col in df.columns:
+            return df.drop(columns=[col])
+    return df

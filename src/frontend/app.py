d481ca2d4f4a46f6cf4c781c806b27f1220114a0
gersongerardcruz <ggerardcruz@gmail.com>
2023-02-23 10:56:56 +0800

    update frontend

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -38,21 +38,6 @@ def app():
     # Set app title
     st.title('End-to-End Network Intrusion Detection Model')
 
-    # Set project information
-    st.write("### Project Information")
-    st.write("""Network intrusion detection is a critical aspect of cybersecurity that helps organizations safeguard their networks and sensitive 
-                data from cyber attacks. As more businesses and individuals rely on technology for daily operations and communication, the risk 
-                of cyber attacks becomes increasingly prevalent. Hackers and malicious actors use various techniques to exploit vulnerabilities 
-                in networks and gain unauthorized access to confidential information, which can result in financial loss, legal ramifications, and 
-                reputational damage. Network intrusion detection models serve as a powerful defense mechanism against these threats by monitoring 
-                network traffic and identifying potential security breaches. By providing early warning signals of potential attacks, these models 
-                allow security teams to take immediate action and prevent or mitigate the impact of a cyber attack.""")
-    st.write("""This is an end-to-end deployment of a network intrusion classification model trained on [Kaggle's Network Intrusion Dataset](https://www.kaggle.com/datasets/sampadab17/network-intrusion-detection). 
-                AutoML and Mlflow were used to track and train a best model and FastAPI and Streamlit were used to create frontend and backend components that use 
-                the best model generated to generate predictions from an uploaded dataset.""")
-    st.write("""For more information about the project, refer to the [github project repo](https://github.com/gersongerardcruz/network_intrusion_detection) where 
-                I documented the entire process in-depth.""")
-
     # Define a placeholder for the uploaded file data
     file_data = None
 
@@ -60,13 +45,13 @@ def app():
     predictions = None
 
     # Use st.file_uploader to get a file from the user
-    st.write("### Choose file for classification here")
+    st.caption("Choose file for classification here")
     file = st.file_uploader('', type=['csv', 'xlsx'], label_visibility='collapsed')
 
     # If a file was uploaded
     if file is not None:
 
-        st.markdown("##### Check if your uploaded data is correct: ")
+        st.caption("Check if your uploaded data is correct: ")
 
         # Use pandas to read the file into a dataframe
         file_data = pd.read_csv(file) if file.name.endswith('.csv') else pd.read_excel(file)
@@ -107,6 +92,7 @@ def app():
             mime='application/json'
         )
 
+        st.markdown("""###### For more information about the project, refer to the [github project repo](https://github.com/gersongerardcruz/network_intrusion_detection) where I documented the entire process in-depth.""")
 # Run the Streamlit app
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
